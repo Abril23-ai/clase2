b450d05aa03c32ef1a11d8651d371eddb84f8b09
Abril23-ai <ariannitasuazo@gmail.com>
2025-03-27 09:14:47 -0600

    subir

--- js/scriptss.js
+++ js/scriptss.js
@@ -1,64 +1,57 @@
-
-
-
 document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("miboton").addEventListener("click", function() {
-        mostrarmenssaje();
+    document.getElementById("mi_boton").addEventListener("click", function() {
+      mostrar_menssaje();
     });
 
-    document.getElementById("miboton2").addEventListener("click", function() {
-        cambiarcolor();
+    document.getElementById("mi_boton2").addEventListener("click", function() {
+       cambiar_color();
     });
 });
 
 
-function mostrarmenssaje() {
-   
-    // declaracion de variables
+function mostrar_menssaje() 
+{
+   // declaracion de variables
     var dato1;
-    var dato2;
-    // asignacion de id a las  variables
-    dato1 = document.getElementById ("input1").value;
-    dato2 = document.getElementById ("input2").value;
+    var dato2; 
+  // asignacion de id a las  variables
+   dato1 = document.getElementById("input1").value;
+   dato2 = document.getElementById("input2").value;
 
-   
-
-    if ( dato1.trim === ' '|| dato2.trim === ' ')
-            alert(por_favor_llenar_sus_datos);        
-    else
-    {
+  if (dato1.trim() ===""|| dato2.trim() ==="" )
+      {
+        alert("por_favor_llenar_sus_datos");  
+      }      
+  else
+     {
       // construir mensaje
-    var mensaje = "los datos son" + " " + dato1 + " y " + dato2;
-    // mostrar mensaje
-        (document.getElementById("resultado").innerText = mensaje);
-    } 
+      var mensaje = "los datos son" +" " + dato1 +" y "+ dato2;
+      // mostrar mensaje
+      document.getElementById("resultado").innerText = mensaje;
+     } 
  
 }
 
-function cambiarcolor() {
+function cambiar_color() 
+{
 
-    var elemento; 
+  var elemento = document.getElementById("contenedor");
 
-    elemento = document.getElementById("container");
-
-   if (document.body.style.backgroundColor === "black")
-    {
-        document.body.style.backgroundColor = "white"
-        elemento.style.position="adsolute";
-        elemento.style.width = "150px";
+  if (document.body.style.backgroundColor === "black")
+       {
+         document.body.style.backgroundColor = "white";
+         elemento.style.width = "150px";
+         elemento.style.heigth = "150px";
+         document.body.style.backgroundColor="red";
+       } 
+   else 
+   {
+       document.body.style.backgroundColor = "black";
+       elemento.style.width = "150px";
        elemento.style.heigth = "150px";
-      document.body.style.backgroundColor = "red"
-    }
-    
-   
-    else {
-        document.body.style.backgroundColor === "black" 
-        elemento.style.width = "150px";
-        elemento.style.heigth = "150px";
-        document.body.style.backgroundColor === "white"   
-    }
+       document.body.style.backgroundColor="blue";
+   }
 
-    
 }
     
 
